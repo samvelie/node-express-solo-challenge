4c306f695015b76468b362a4223f454d4b4687da
Kris Szafranski <kdszafranski@gmail.com>
2017-02-27 11:13:03 -0600

    updated server with static files and basic serving

--- server/app.js
+++ server/app.js
@@ -1,3 +1,9 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = 3000;
+
+
 // initial jokes provided by the client
 var jokes = [
   {
@@ -9,5 +15,26 @@ var jokes = [
     whoseJoke: "Kris",
     jokeQuestion: "How many software engineers does it take to change a lightbulb?",
     punchLine: "None! That's a hardware problem!"
-  }  
+  },
+  {
+    whoseJoke: "Scott",
+    jokeQuestion: "Went to the zoo the other day. It only had one dog in it.",
+    punchLine: "It was shih tzu."
+  }
 ];
+
+// static file requests
+app.use(express.static('server/public'));
+
+// routes
+
+
+// Send index.html file
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve('server/public/views/index.html'));
+});
+
+// Start the server!
+app.listen(port, function() {
+  console.log("Node listening on port " + port);
+});
