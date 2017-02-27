f3d3034ea416ae71db5bb7d4e8f33729cd925cc1
Kris Szafranski <kdszafranski@gmail.com>
2017-02-27 10:29:38 -0600

    updated jokes

--- server/app.js
+++ server/app.js
@@ -1,18 +1,13 @@
 // initial jokes provided by the client
 var jokes = [
   {
-    whoseJoke: "Huck",
-    jokeQuestion: "What's the difference between roast beef and pea soup?",
-    punchLine: "Anyone can roast beef."
+    whoseJoke: "Luke",
+    jokeQuestion: "Friends are like snow flakes...",
+    punchLine: "If you pee on them they disappear."
   },
   {
     whoseJoke: "Kris",
     jokeQuestion: "How many software engineers does it take to change a lightbulb?",
     punchLine: "None! That's a hardware problem!"
-  },
-  {
-    whoseJoke: "Luke",
-    jokeQuestion: "Friends are like snow flakes...",
-    punchLine: "If you pee on them they disappear."
-  }
+  }  
 ];
